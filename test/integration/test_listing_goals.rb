ae286bc4bf7b6b416fa2a205f6bd6eafbb01b219
Matthew Reuther <matthew.reuther@gmail.com>
2015-05-13 21:46:44 -0500

    created test to test listing multiple goals

--- test/integration/test_listing_goals.rb
+++ test/integration/test_listing_goals.rb
@@ -6,16 +6,29 @@ class TestListingGoals < Minitest::Test
   shell_output = ""
   expected_output = ""
   IO.popen('./sleep_tracker manage', 'r+') do |pipe|
-    expected_output = <<EOS
-1. Add a goal
-2. List all goals
-3. Exit
-EOS
+    expected_output << main_menu
     pipe.puts "2"
     expected_output << "No goals found. Add a goal.\n"
     pipe.close_write
     shell_output = pipe.read
   end
   assert_equal expected_output, shell_output
+  end
+
+  def test_listing_multiple_goals
+    skip
+    create_goal("8 hours")
+    create_goal("6 hours")
+    shell_output = ""
+    expected_output = ""
+    IO.popen('./sleep_tracker manage', 'r+') do |pipe|
+      expected_output << main_menu
+      pipe.puts "2" # List all goals
+      expected_output << "1. 8 hours\n"
+      expected_output << "2. 6 hours\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected_output, shell_output
   end
 end
